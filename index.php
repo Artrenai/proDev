9f38c2e54d9982d0d1ec01546adae099609213a7
Godfrey Davis <gdavis42@cnm.edu>
2017-08-15 16:01:15 -0600

    finished pro dev week 6 null resume 2nd Draft

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Godfrey Davis - Pro Dev</title>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
+
+	</head>
+
+	<body>
+		<h1>
+			Profesional Development
+		</h1>
+
+		<h2> Qualitative And Quantitative Skills</h2>
+		<dl>
+			<dt>
+			<dt>
+				Qualitative
+			</dt>
+			<dd>
+				I have a gungho attitude. Im self aware and can acknowledge my own flaws when not meeting expectations. When
+				i hit a challenge it doesn't take me long to recover and make a proactive decision
+				Im genuinly a kind person, for the most part im able to be positive in circumstances that might provoke
+				otherwise.
+			</dd>
+			<dt>
+				Quantitative
+			</dt>
+			<dd>
+				I have experience in project management from the print industry. Was responsible for 8 people and their
+				training to create a more efficient workflow and faster turn around for print projects.
+				I have 7 years experience in Adobe suite print applications. Photoshop, Illustrator, Indesign, Acrobat
+				professional.Was part of genomic research initiative to database mycobacteriophages for
+				bio-informatics.
+			</dd>
+		</dl>
+
+		<h2>
+			Why How And What
+		</h2>
+		<dl>
+			<dt>Why</dt>
+			<dd>
+				Growing up, I had little exposure to STEM and technology. Everything I've done with my life so far has been
+				for the future goal of making sure under represented youth atleast
+				know that this world of science and technology exist. I hope to be an example that someone of my demographic
+				can do this.
+			</dd>
+
+			<dt>How</dt>
+			<dd>
+				Im an Engineering Science Major and to make sure I dont leave College not knowing or using programing
+				skills. Im putting myself through an intensive "hands on" coding bootcamp.
+			</dd>
+			<dt>What</dt>
+			<dd>
+				Become a full-time senior developer.. become an Engineer be Involved in outreach programs.
+			</dd>
+		</dl>
+		<h2>Elevator Speech</h2>
+		<p>
+			hi my name is Godfrey. I want to be as involved as possible in this community of technology. Im hoping to learn
+			and experience first hand as much as possible. Im currently using
+			Coding and software development as my access into as much as i can. So that i can then take that experience and
+			inspire more people from communities where the influence of technology
+			and science hasnt mnet standard because social barriers which results in cultural lag.
+		</p>
+		<div>
+			<ul>
+				<li><em>My Buddy's name is Leonora Sanchez-Rees</em></li>
+				<li><em>Im a tactile learner according to the test</em></li>
+			</ul>
+		</div>
+		<h2>My Social Networks</h2>
+		<ul>
+			<li><a href="https://www.linkedin.com/in/godfrey-davis-4a661365/">Linkedin</a></li>
+			<li><a href="https://twitter.com/artrenai">Twitter</a></li>
+			<li><a href="https://www.artrenai.com">Personal Website</a></li>
+		</ul>
+		<h2>Resume 1st Draft</h2>
+		<a href="docs/resume.pdf" download="resume">Resume</a>
+		<h2>Forbidden Island Assesment</h2>
+		<dl>
+			<dt><strong>Jack:</strong></dt>
+			<dd>Shows initiative was the first to start to read instructions but is a good team player.... I kinda took the
+				directions from him and started reading<br></dd>
+			<dt><strong>Tim:</strong></dt>
+			<dd>Cooperation was well when it came to strategy he was more vocal in how to help the other players</dd>
+			<dt><strong>Marty:</strong></dt>
+			<dd>A great sense of humor.. kept us laughing... more importantly he focused on the most efficient way to play
+				the game.
+			</dd>
+		</dl>
+		<h2>Buddy Interview</h2>
+		<dl>
+			<dt>Internal</dt>
+			<dd>
+				I have a hard time believing in myself. It becomes a block for me. I have to motivate myself and not think
+				of all the negativity
+			</dd>
+			<dt>External</dt>
+			<dd>Im new to New Mexico, I dont many people. Tomorrow Im going to see some rentals. Its messing up my goals
+				for the next couple of months. Other than that I've
+				been blessed. Im a nice person, a genuinely nice person. I'm very laid back and empathetic. When people go
+				in a negative direction, I go in a positive direction Im able to understand
+				a situation and be empathetic. All the people who dont know me have told me im awesome. The people who do
+				know me devalue how hard i work in turn i devalue my own progress because the results are not immediate
+			</dd>
+		</dl>
+		<h2>Resume 2nd Draft</h2>
+		<a href="#" download="Resume_2">Resume</a>
+	</body>
+	<footer>
+
+	</footer>
+</html>
